0f0cf841cac3c4ae462fb81ce7e34f54cadac429
Yaspesberro <yaspesberro@gmail.com>
2018-02-07 17:46:11 -0800

    Adding two questions in JS, adding more content to HTML

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+var correctAnswers = 0; //Out of 7 possible
 
 var userName = prompt('Hi, what is your name?');
 alert('Hi, ' + userName + ' nice to meet you, Im going to ask you a few questions about myself. Please answer questions with either yes/no or y/n.');
@@ -8,7 +9,8 @@ console.log('Q: Do you think I have any cats? ' + userName + ' answered ' + answ
 
 if(answer1 === 'yes' || answer1 === 'y') {
     alert('You\'re correct ' + userName + '! I have three cats.');
-   } else if(answer1 === 'no' || answer1 === 'n') {
+    correctAnswers++;
+} else if(answer1 === 'no' || answer1 === 'n') {
     alert('You\'re wrong ' + userName + '! I have three cats.');
 } else {
     alert('Sorry, you did not enter a valid answer. On to the next question..');
@@ -19,6 +21,7 @@ console.log('Q: Do you think I have any siblings? ' + userName + ' answered ' + 
 
 if(answer2 === 'yes' || answer2 === 'y') {
     alert('You\'re correct ' + userName + '! I have two brothers.');
+    correctAnswers++;
 } else if(answer2 === 'no' || answer2 === 'n') {
     alert('You\'re wrong ' + userName + '! I have two brothers.');
 } else {
@@ -32,6 +35,7 @@ if(answer3 === 'yes' || answer3 === 'y'){
     alert('You\'re wrong ' + userName + '. I actually grew up in Luxembourg.');
 } else if(answer3 === 'no' || answer3 === 'n') {
     alert('You\'re correct ' + userName + '. I actually grew up in Luxembourg.');
+    correctAnswers++;
 } else {
     alert('Sorry, you did not enter a valid answer. On to the next question..');
 }
@@ -41,6 +45,7 @@ console.log('Q: Do you think I like pizza? ' + userName + ' answered ' + answer4
 
 if(answer4 === 'yes' || answer4 === 'y') {
     alert('Obviously I like pizza..');
+    correctAnswers++;
 } else if(answer4 === 'no' || answer4 === 'n') {
     alert('Cmon ' + userName + '.. who doesn\'t like pizza?');
 } else {
@@ -52,8 +57,61 @@ console.log('Q: Do you think I speak more than one language? ' + userName + ' an
 
 if(answer5 === 'yes' || answer5 === 'y') {
     alert('You\'re correct ' + userName + '! I speak both English and Luxembourgish.');
+    correctAnswers++;
 } else if(answer5 === 'no' || answer5 === 'n') {
     alert('You\'re wrong ' + userName + '. I speak both English and Luxembourgish.');
 } else {
     alert('Sorry, you did not enter a valid answer.');
 }
+
+
+
+alert('Try to guess how old I am. You have four tries total to guess it correctly.');
+
+for(var guesses = 4; guesses > 0; guesses--) {
+    var userGuess = prompt('Guesses left: ' + guesses + '.');
+
+    //If input is anything but a number, exit out of the loop without adding a guess
+    if(isNaN(userGuess) || userGuess === '') { 
+        alert("Invalid input, please enter a number.");
+        console.log('Invalid input: guess was NaN.');
+        guesses++; //Adds guess since it wasn't a valid input 
+    } else {
+        if(userGuess == 20) { 
+            alert('You guessed correct! I\'m 20 years old. You had ' + (guesses - 1) + ' guesses remaining.');
+            correctAnswers++;
+            break;
+        } else {
+            if(userGuess < 20) {
+                alert('Not quite.. I\'m older than ' + userGuess + '.');
+            } else if(userGuess > 20) {
+                alert('Not quite.. I\'m younger than ' + userGuess + '.');
+            }
+        } 
+    }
+}
+
+
+alert('Guess one of the states I\'ve visited outside of Washinton, reply with state name or abbreviation (ex. Washington or WA).');
+//Contains State names
+var fullAnswers = ["california", "oregon", "hawaii", "alaska", "idaho", "nevada", "minnesota"];
+//Contains State abbreviations
+var shortAnswers = ["ca", "or", "hi", "ak", "id", "nv", "mn"];
+
+for(var guesses = 6; guesses > 0; guesses--) {
+    var userInput = prompt('Guesses left: ' + guesses).toLowerCase().trim();
+
+    for(var i = 0; i < 7; i++){
+        if(userInput === fullAnswers[i] || userInput === shortAnswers[i]) {
+            alert('Yes, I have indeed been to ' + fullAnswers[i]);
+            correctAnswers++;
+            i = 7;
+            guesses = 0;
+        } else if(i === 6) {
+            alert('Nope, I have never been to ' + userInput);
+        }
+    }
+}
+
+//Shows how many questions got answered correctly 
+alert('You managed to get ' + correctAnswers + '/7 questions right.');
